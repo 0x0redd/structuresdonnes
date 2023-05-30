d25b764a185860330374ee291e5ff30992a686bd
0x0redd <0x0red.me@gmail.com>
2023-05-30 19:45:29 +0100

    abr

---
+++ ABR.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define N 50 /*N= nombre de somet */
+
+
+
+typedef struct arb{
+    unsigned int key;
+    struct node *left; //gauche 
+    struct node *right; // droite
+}arb;
+
+void ajouterarb(arb **tree, unsigned int key){
+    arb *auxnode;
+    arb *tmparb = *tree;
+
+    arb *elem = (abr *) malloc (1*sizeof(abr));
+    elem->key = key;
+    elem->left = NULL;
+    elem->right = NULL;
+
+    if(tmparb)
+        do {
+            tmparb =
+        }
+
+}
+
+
+main(){
+    
+}
--- Arbre.c
+++ Arbre.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include "pile.c"
 
 #define N 20 
 typedef int sommet [N];
@@ -10,30 +11,87 @@ main(){
     sommet frere={-1,-1,4,5,-1,-1,7,-1,9,-1,11,-1,13,-1};
     sommet pere={-1,-1,1,2,1,2,4,4,5,5,7,7,9,9};
 }
+// calcul du nb de feuills d'un arbre 
 
-int nbr_feuilles(sommet n,valeur val){
-    int i,nbf=0;
-    for (i=1;i<N;i++)
-        if (n[i]==-1) {nbf++; printf("%s\n",val[i]);}
-        t=fils[sommet];
-        while(t!=-1){
-
-            printf("%s",val[t]);
-            t=frere[t];
+int nbr_feuilles(sommet n, valeur v){
+    int i, nbf=0;
+    for(i=0;i<N;i++){
+        if (n[i]==-1) {
+            nbf++;
+            printf("%s\n"val[i]);
         }
-    
+    }
+    return nbf;
 }
 
-char *ind_pere(char *s , sommet fils , sommetfrere ,valeur val){
-    int i,t;
-    for(i=1;i<N;i++) if(!strcmp(s,val[i])){t=i break; }
-    int pere = -1 ;
-    if (t == 1 ) {printf("pas de pere \n"); getchar();exit(0);}
-    while (pere == -1 )
-    {
-        for(i=1;i<N;i++) if (t==fils[i]) pere=i;
-        for(i=1;i<N;i++) if (t==frere[i]) t=i;
+// trouver les element de fi
+
+void phi(char *s, sommet fils, sommet frere ,valeur val){
+    int sommet , i , t;
+    for(i=1;i<N;i++)
+        if(!strcmp(s,val[i])){sommet=i; break;}
+    t=fils[sommet];
+    while(t!=-1){
+        printf("%s",val[t]);
+        t=frere[t];
     }
-    return val [pere];
-    
+}
+
+//trouver l'indice du père de t
+
+char *ind_pere(char *s,sommet fils , sommet frere , valeur val){
+    int i , t;
+    for(i=1;i<N;i++)
+        if(!strcmp[s,val[i]]) {t=i; break;}
+    int pere=-1;
+    if(t==1) {
+        printf("pas de pere \n");
+        getchar();
+        exit(0);
+    }
+
+    while (pere== -1 ){
+        for(i=1;i<N;i++){
+            if(t==fils[i]) pere = i;
+        }
+        for(i=1;i<N;i++){
+            if (t==frere[i]) t=i;
+        }
+    }
+    return val[pere];
+}
+
+// parcours ##recursif## en preordre 
+
+void preordre(int s,sommet fils,sommet frere,valeur val){
+    int t;
+    printf("%s",val[s]);
+    t = fils[s];
+    while(t=-1){
+        preordre(t,fils,frere,val);
+        t=frere[t];
+    }
+}
+
+//parcours ##itératif## en preordre
+void preordre_it(int s,sommet fils,sommet frere,valeur val){
+    pile p;
+    int x;  
+    creerp(&p);
+    ajouterp(-1,&p);
+    while(!videp(p)){
+        printf("%s"val[s]);
+        if(fils[s]!=-1){
+            if(frere[s]!=-1) ajouterp(frere[s],&p);
+            s=fils[s];
+        }
+        else 
+            if (frere[s]!=-1) s=frere[s];
+            else{
+                x=valeurp(p);
+                supprimer(&p);
+                if (x==-1) break;
+                else s=x;
+            }
+    }
 }
